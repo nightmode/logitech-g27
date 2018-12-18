f07c09d22deb634593b2eb29665a0367e596d113
Daniel Gagan <forestmist@users.noreply.github.com>
2018-12-17 22:38:46 -0800

    change detect logic

--- code/index.js
+++ code/index.js
@@ -162,11 +162,14 @@ function findWheel() {
     var devicePath = ''
 
     for (var i in devices) {
-        // devices[i].product will be set to 'G27 Racing Wheel' on Windows and Mac. Linux will not have this key.
+        // devices[i].vendorId seems to be the only completely reliable property on each OS.
+        // devices[i].productId can not be trusted and can sometimes be wildly different.
+        // devices[i].product should be set to 'G27 Racing Wheel'.
         // devices[i].interface should be 0 on Windows and Linux.
         // devices[i].usagePage should be 1 on Windows and Mac.
-        if (devices[i].vendorId === 1133 && (devices[i].productId === 49812 || devices[i].productId === 49819) &&
-        (devices[i].interface === 0 || devices[i].usagePage === 1)) {
+        if (devices[i].vendorId === 1133 &&
+            (devices[i].productId === 49812 || devices[i].productId === 49819 || devices[i].product === 'G27 Racing Wheel') &&
+            (devices[i].interface === 0 || devices[i].usagePage === 1)) {
             devicePath = devices[i].path
             break
         }
